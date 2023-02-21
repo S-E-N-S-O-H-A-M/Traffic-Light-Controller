02d031e7ceb2021a804a1939b701c24d87220d78
S-O-H-A-M <65547096+S-O-H-A-M@users.noreply.github.com>
2023-02-21 14:47:55 +0530

    Update traffic_light_controller_test.v

--- traffic_light_controller_test.v
+++ traffic_light_controller_test.v
@@ -2,14 +2,16 @@
 module traffic_light_controller_test;
 //Inputs
 reg clk;
+reg v_detect;
 reg rst;
 
 //Outputs
-wire [1:0] NS_light;
-wire [1:0] EW_light;
+wire [3:0] NS_light;
+wire [3:0] EW_light;
 //Instantiate the Unit Under Test
 traffic_light_controller uut(
     .clk(clk),
+    .v_detect(v_detect),
     .rst(rst),
     .NS_light(NS_light),
     .EW_light(EW_light)
@@ -23,15 +25,15 @@ initial begin
 
     //Initialize Inputs
 
-    clk=0;rst=0;
+    clk=0;rst=1;
 
     //Wait for 10 ns for global reset to finish
 
-    #10 rst=1;
-    #200;
-    //$stop();
+    #10 rst=0;v_detect=1;
+    #300;v_detect=0;
+    #50;
     $finish();
-    
+
     //Add stimulus here
 
 end
